84eff75a8c1efc6b9f513bc0dcc8e4493a3c62da
Bruno Carrard <brunocarrardoliveira@gmail.com>
2024-01-18 16:05:20 -0300

    add scan to sales order nr

--- src/components/FormSalesOrder.vue
+++ src/components/FormSalesOrder.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="w-1/2 h-96 bg-black md:mt-22 lg:mt-44 rounded-xl flex flex-col items-center justify-center gap-4 border-4 border-red-500">
         <div class="w-48 flex flex-col gap-2">
-            <div>
-                <h1 class="text-white">Scan Sales Order:</h1>
-                <input class="w-full border rounded h-8" placeholder="Click here" v-model="ordNr" :disabled="loading">
+            <div class="flex flex-col gap-2">
+                <h1 class="text-white font-bold">Scan Sales Order:</h1>
+                <div id="reader" class="text-white" style="width: 200px;"></div>
+                <input class="w-full border rounded h-8 text-center" placeholder="Scan will appear here" v-model="ordNr" :disabled="loading">
             </div>
         </div>
-        <button @click="requestSalesOrder" :disabled="loading"
+        <button @click="requestSalesOrder" :disabled="loading" v-if="ordNr != ''"
             class="bg-white rounded w-48 text-grey font-bold h-8">
             <label v-if="!loading" class="cursor-pointer">Confirm</label>
             <label v-if="loading" class="animate-pulse">Searching...</label>
@@ -16,12 +17,26 @@
 
 <script>
     import axios from "axios";
+    import { Html5QrcodeScanner } from "html5-qrcode";
     export default {
         data() {
             return {
                 ordNr: '',
                 loading: false
             }
+        },
+        mounted() {
+            const html5QrCode = new Html5QrcodeScanner(
+                    "reader", { fps: 5, qrbox: 350, aspectRatio: 1, focusMode: "continuous" }
+                );
+                html5QrCode._fileInput = null
+                html5QrCode.render(
+                    (decodedText, decodedResult) => {
+                    // do something with the decoded text
+                    this.ordNr = decodedText;
+                    console.log('sacneado')
+                    },
+                );
         },
         methods: {
             async requestSalesOrder() {
