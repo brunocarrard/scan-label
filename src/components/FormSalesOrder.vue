eaafaf3905510f009847e6b66db3fa48fcf10da3
Bruno Carrard <brunocarrardoliveira@gmail.com>
2024-02-29 21:25:40 -0300

    removing test app

--- src/components/FormSalesOrder.vue
+++ src/components/FormSalesOrder.vue
@@ -3,7 +3,6 @@
         class="sm:w-1/2 h-32 sm:h-96 bg-black px-4 sm:px-0 mt-44 sm:mt-10 rounded-xl flex flex-col items-center justify-center gap-4 border-4 border-red-500">
         <div class="w-48 flex flex-col gap-2">
             <div class="flex flex-col gap-2">
-                <h1 class="text-white font-bold">TEST APP</h1>
                 <h1 class="text-white font-bold">Scan Sales Order:</h1>
                 <div id="reader" class="text-white hidden sm:block" style="width: 200px;"></div>
                 <input class="w-full border rounded h-8 text-center" placeholder="Scan will appear here" v-model="ordNr"
