f4fc6c2e2834125e5dc6b3336addc3e617c023bc
Bruno Carrard <brunocarrardoliveira@gmail.com>
2024-03-15 15:18:56 -0300

    atts to BOm

--- src/components/FormSalesOrder.vue
+++ src/components/FormSalesOrder.vue
@@ -49,7 +49,7 @@ export default {
             else {
                 this.loading = true;
                 try {
-                    const response = await axios.get('http://127.0.0.1:5000', {
+                    const response = await axios.get('https://192.168.0.154:4020', {
                         params: {
                             value: this.ordNr,
                         }
